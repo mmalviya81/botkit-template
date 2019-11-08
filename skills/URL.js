406afad04f8335e774aac6c1b3e1f1f80a75ac15
mmalviya81 <57469061+mmalviya81@users.noreply.github.com>
2019-11-08 10:21:30 -0800

    Update URL.js

--- skills/URL.js
+++ skills/URL.js
@@ -3,7 +3,7 @@
 //
 module.exports = function (controller) {
 
-    controller.hears([/^URL/], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^URL$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
             convo.say('This is a Botkit conversation sample.');
