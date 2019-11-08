8192dfd44c1d3abdcd34248993cb3667c1365b33
mmalviya81 <57469061+mmalviya81@users.noreply.github.com>
2019-11-08 10:17:02 -0800

    Update URL.js

--- skills/URL.js
+++ skills/URL.js
@@ -3,13 +3,13 @@
 //
 module.exports = function (controller) {
 
-    controller.hears([/^color$/], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^URL/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
             convo.say('This is a Botkit conversation sample.');
 
-            convo.ask('What is your favorite color?', function (response, convo) {
-                convo.say("Cool, I like '" + response.text + "' too!");
+            convo.ask('Which tool URLs you are looking for?', function (response, convo) {
+                convo.say("Here are the URL '" + response.text + "' https://www.google.com/");
                 convo.next();
             });
         });
